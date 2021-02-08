1e2b01f51ac869902fec07fda832caf6be3b146d
root <root@DESKTOP-35T72BQ.localdomain>
2021-02-08 22:10:26 +0800

    support jdk11

--- src/main/java/net/onebean/component/redis/RedisConfig.java
+++ src/main/java/net/onebean/component/redis/RedisConfig.java
@@ -5,44 +5,41 @@ import net.onebean.core.form.Parse;
 import net.onebean.util.PropUtil;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-import redis.clients.jedis.JedisPoolConfig;
 
 @Service
 public class RedisConfig {
-  
-	private static final Logger logger = (Logger) LoggerFactory.getLogger(RedisConfig.class);
-	private static final String HOST_NAME_KEY = "spring.redis.hostName";
-	private static final String PORT_KEY = "spring.redis.port";
-	private static final String PASSWORD_KEY = "spring.redis.password";
-	private static final String DB_SELECT_KEY = "spring.redis.database";
 
-    @Bean(name = "getRedisConfig")
-    @ConfigurationProperties(prefix="spring.redis")
-    public JedisPoolConfig getRedisConfig(){
-        return new JedisPoolConfig();
-    }  
-      
-    @Bean(name = "getConnectionFactory")
-    public JedisConnectionFactory getConnectionFactory(@Qualifier("getRedisConfig")JedisPoolConfig getRedisConfig){
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setPoolConfig(getRedisConfig);
-        factory.setHostName(PropUtil.getInstance().getConfig(HOST_NAME_KEY,PropUtil.PUBLIC_CONF_REDIS));
-        factory.setPort(Parse.toInt(PropUtil.getInstance().getConfig(PORT_KEY,PropUtil.PUBLIC_CONF_REDIS)));
-        factory.setPassword(PropUtil.getInstance().getConfig(PASSWORD_KEY,PropUtil.PUBLIC_CONF_REDIS));
-        factory.setDatabase(Parse.toInt(PropUtil.getInstance().getConfig(DB_SELECT_KEY,PropUtil.PUBLIC_CONF_REDIS)));
-        logger.info("JedisConnectionFactory bean init success.");
-        return factory;  
-    }  
-      
-      
-    @Bean(name = "getRedisTemplate")
-    public RedisTemplate<?, ?> getRedisTemplate(@Qualifier("getConnectionFactory")JedisConnectionFactory getConnectionFactory){
-        return new StringRedisTemplate(getConnectionFactory);
-    }  
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(RedisConfig.class);
+    private static final String HOST_NAME_KEY = "spring.redis.hostName";
+    private static final String PORT_KEY = "spring.redis.port";
+    private static final String PASSWORD_KEY = "spring.redis.password";
+    private static final String DB_SELECT_KEY = "spring.redis.database";
+
+    @Bean(name = "lettuceConnectionFactory")
+    LettuceConnectionFactory initLettuceConnectionFactory() {
+        String hostName = PropUtil.getInstance().getConfig(HOST_NAME_KEY, PropUtil.PUBLIC_CONF_REDIS);
+        int port = Parse.toInt(PropUtil.getInstance().getConfig(PORT_KEY, PropUtil.PUBLIC_CONF_REDIS));
+        String password = PropUtil.getInstance().getConfig(PASSWORD_KEY, PropUtil.PUBLIC_CONF_REDIS);
+        int database = Parse.toInt(PropUtil.getInstance().getConfig(DB_SELECT_KEY, PropUtil.PUBLIC_CONF_REDIS));
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        redisStandaloneConfiguration.setDatabase(database);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        logger.info("LettuceConnectionFactory bean init success , redis host = {} , redis port ={}", lettuceConnectionFactory.getHostName(), lettuceConnectionFactory.getPort());
+        return lettuceConnectionFactory;
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, String> stringRedisTemplate(@Qualifier("lettuceConnectionFactory") LettuceConnectionFactory lettuceConnectionFactory) {
+        return new StringRedisTemplate(lettuceConnectionFactory);
+    }
 } 
